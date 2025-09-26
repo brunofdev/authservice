0b0b4c507559b9fcb05bf213879d52b692962ff9
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-25 23:14:50 -0300

    configurada  e estruturado o serviço para deploy de teste

---
+++ authservice/src/main/java/com/authservice/authservice/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.authservice.authservice.controller;
+
+import com.authservice.authservice.dto.AuthResponseDTO;
+import com.authservice.authservice.dto.CredentialsDTO;
+import com.authservice.authservice.service.AuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService){
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody CredentialsDTO credentialsDTO){
+        String jwtToken = authenticationService.login(credentialsDTO);
+        AuthResponseDTO responseDTO = new AuthResponseDTO(jwtToken);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+}
---
+++ authservice/src/main/java/com/authservice/authservice/service/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.authservice.authservice.service;
+
+import com.authservice.authservice.dto.CredentialsDTO;
+import com.authservice.authservice.exceptions.InvalidCredentialsException;
+import com.authservice.authservice.jwt.JwtProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AuthenticationService {
+
+    private final WebClient.Builder webClientBuilder;
+    private final JwtProvider jwtProvider;
+
+    @Value("${userservice.api.url}")
+    private String userServiceUrl;
+
+    @Value("${api.internal.secret}")
+    private String internalApiSecret;
+
+    public AuthenticationService(WebClient.Builder webClientBuilder, JwtProvider jwtProvider){
+        this.webClientBuilder = webClientBuilder;
+        this.jwtProvider = jwtProvider;
+    }
+
+    private void validateCredentialsWithUserService(CredentialsDTO credentials){
+        WebClient webClient = webClientBuilder.baseUrl(userServiceUrl).build();
+        webClient.post()
+                .uri("/userservice/internal/validate-user-credential")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header("X-Internal-Secret", internalApiSecret)
+                .body(Mono.just(credentials), CredentialsDTO.class)
+                .retrieve()
+                .onStatus(
+                        status -> status.is4xxClientError(),
+                        response -> Mono.error(new InvalidCredentialsException("Credenciais inválidas fornecidas."))
+                )
+                .toBodilessEntity()
+                .block();
+    }
+
+    public String login(CredentialsDTO credentials) {
+        validateCredentialsWithUserService(credentials);
+        return jwtProvider.generateToken(credentials.getUserName());
+    }
+
+
+
+
+
+
+}
